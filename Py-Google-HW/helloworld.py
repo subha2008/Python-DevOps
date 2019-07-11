df092a45ba27e797de30f8da2b130452700c7a65
krisare-revathi <52771000+krisare-revathi@users.noreply.github.com>
2019-07-11 12:06:20 +0530

    Update helloworld.py

--- Py-Google-HW/helloworld.py
+++ Py-Google-HW/helloworld.py
@@ -3,3 +3,4 @@ if __name__ == '__main__':
     print('Subha - IBM - Jul 11- 11:10 am')
     print('Excellent Session Subhashini')
     print('I have edited your code')
+    print("Hai Subhashini, Mr.Nagendran class is excellent and taking class patiently, slowly and clear with concept")
